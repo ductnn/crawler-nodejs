f284bd06e9bf27697531c67bce1be41811c067a1
DuctnNetwork <trannhuduc531998@gmail.com>
2020-04-18 23:50:48 +0700

    Update

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const port = 3000;
 const express = require('express');
 const cheerio = require('cheerio');
 const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 
 const app = new express();
+const siteUrl = "https://coders-x.com/blog/";
 
 // Middleware
 app.use(express.static("public"));
@@ -13,19 +15,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+
 app.get("/", (req, res) => {
-    request("https://laptrinhcuocsong.com", (err, response, body) => {
+    request(siteUrl, (err, response, body) => {
         if (err) {
-            console.log(err)
+            console.log(err);
+            res.render("index", { html: "Loading wrong!!!" });
         } else {
-            fs.writeFile('./logs/input.txt', body, (err) => {
-                if (err) {
-                    console.log(err);
-                };
-                console.log("write success");
-            });
+            // fs.writeFile('./logs/input.txt', body, (err) => {
+            //     if (err) {
+            //         console.log(err);
+            //     };
+            //     console.log("write success");
+            // });
+
+            $ = cheerio.load(body);
+            var list = $(body).find("article#post");
             res.render("index", {
-                html: body
+                html: list
             });
         };
     });
